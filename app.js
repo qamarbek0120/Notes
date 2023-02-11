0f68cfd4b208761a1c4ca290c9eb29cb8f60255e
student0120 <115689656+student0120@users.noreply.github.com>
2023-02-11 22:42:24 +0500

    improved

--- app.js
+++ app.js
@@ -4,11 +4,26 @@ const app = express()
 
 app.set('view engine', 'pug')
 app.use('/static', express.static('public'))
+app.use(express.urlencoded({extended: false}))
 app.get('/', (request, response) => {
     response.render('home')
 })
 app.get('/create', (request, response)=>{
     response.render('create')
+})
+app.post('/create', (request, response)=>{
+    const title = request.body.title
+    const description = request.body.description
+    if (title.trim() === '' && description.trim() === ''){
+        response.render('create', { error: true })
+    }
+})
+const notes = ['Some appropriate title', 'Some appropriate title']
+app.get('/notes', (request, response)=>{
+    response.render('notes', {notes:notes})
+})
+app.get('/notes/detail', (request, response)=>{
+    response.render('detail')
 })
 
 app.listen(8000, err =>{
